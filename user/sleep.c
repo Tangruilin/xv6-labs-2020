ebe5828213b051870639ff2f2b7c509e563308fa
Tangruilin <trl@hust.edu.cn>
2021-06-13 13:28:26 +0800

    sleep_finsh

--- user/sleep.c
+++ user/sleep.c
@@ -1,7 +1,7 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-
+// sleep测试完成，难度洗漱:简单
 // 这部分就直接复制一下，这里是对系统调用的考察，难度不是很大
 int     //维持这里面的代码风格
 main(int argc, char const *argv[])
